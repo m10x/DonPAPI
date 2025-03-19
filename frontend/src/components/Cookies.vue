cd9935bc9de6d2c659cefcb67e644a7d81961e3a
Maximilian Hildebrand <github@m10x.de>
2025-03-19 14:52:04 +0100

    fix date

--- frontend/src/components/Cookies.vue
+++ frontend/src/components/Cookies.vue
@@ -156,7 +156,7 @@ export default {
     },
     formatDate(dateString) {
       if (!dateString) return '';
-      const date = new Date(parseInt(dateString));
+      const date = new Date(parseInt(dateString) / 1000);
       if (isNaN(date.getTime())) return dateString;
       return date.toLocaleString('de-DE', {
         year: 'numeric',
